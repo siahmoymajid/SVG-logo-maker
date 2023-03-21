35ec169c88f1277b6e7a4f41c02843761462de40
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-03-20 20:07:38 -0700

    added shape

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquire = require('inquire');
+const = require ('fs');
+const square = require('./examples/square.svg');
+const circle = require('./examples/circle.svg');
+const triangle = require('./examples/triangle');
+
+
+
+
